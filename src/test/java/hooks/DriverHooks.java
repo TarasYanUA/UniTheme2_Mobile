ff533ab4f0580672b7e20ce51d36481455e18d9a
Taras-Mila <w@gmail.com>
2025-06-19 13:01:55 +0300

    Transferred to Java24

--- src/test/java/hooks/DriverHooks.java
+++ src/test/java/hooks/DriverHooks.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DriverHooks {
-    public static final String BASIC_URL = "https://trs.test.abt.team/4183ultru/admin.php?dispatch=addons.manage";
+    public static final String BASIC_URL = "https://trs.test.abt.team/4184ultru_unitheme2/admin.php?dispatch=addons.manage";
 
     public DriverHooks() {super();}
 
@@ -27,7 +27,6 @@ public class DriverHooks {
         WebDriver driver = new ChromeDriver(chromeOptions);
         WebDriverRunner.setWebDriver(driver);   // Устанавливаем созданный драйвер как текущий драйвер для Selenide
         open(BASIC_URL);
-        //Configuration.timeout = 2000; //Общая задержка
         Configuration.screenshots = true; //делаем скриншоты при падении
         Configuration.savePageSource = false; //не создавать html файлы при создании скриншотов
 

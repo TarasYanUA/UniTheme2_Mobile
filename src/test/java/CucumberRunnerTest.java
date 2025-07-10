fc7ac2ef22ccb2349559fa8c906831adbd03c1b9
Taras-Mila <w@gmail.com>
2025-07-10 11:42:27 +0300

    Updated pom file.

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -6,18 +6,18 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"hooks", "steps"},
-        tags = "@BottomStickyPanel_Var3 or @CategoryPage_Grid_Var3 or @CategoryPage_ListWithoutOptions_Var2 or @ProductPage_Var2",
-        //@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3
-        //@ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3
-        //@ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3
+        //tags = "",
+        //tags = "@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3",
+        //tags = "ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3",
+        //tags = "ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3",
 
-        //@BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3
+        //tags = "BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3",
 
-        //@CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3
-        //@CategoryPage_ListWithoutOptions_Var1 or CategoryPage_ListWithoutOptions_Var2
-        //@CategoryPage_CompactList_Var1 or @CategoryPage_CompactList_Var2
+        tags = "CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3",
+        //tags = "CategoryPage_ListWithoutOptions_Var1 or CategoryPage_ListWithoutOptions_Var2
+        //tags = "CategoryPage_CompactList_Var1 or @CategoryPage_CompactList_Var2
 
-        //@ProductPage_Var1 or @ProductPage_Var2 or @ProductPage_Var3
+        //tags = "ProductPage_Var1 or @ProductPage_Var2 or @ProductPage_Var3
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json"}
 )
 public class CucumberRunnerTest {

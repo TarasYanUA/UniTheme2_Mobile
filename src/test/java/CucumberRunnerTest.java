419c21654ab05b5e4471865a4f09e43b71137219
Taras-Mila <w@gmail.com>
2025-07-10 11:53:03 +0300

    Updated pom file.

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -8,16 +8,16 @@ import io.cucumber.junit.Cucumber;
         glue = {"hooks", "steps"},
         //tags = "",
         //tags = "@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3",
-        //tags = "ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3",
-        //tags = "ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3",
+        //tags = "@ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3",
+        //tags = "@ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3",
 
-        //tags = "BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3",
+        //tags = "@BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3",
 
-        tags = "CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3",
-        //tags = "CategoryPage_ListWithoutOptions_Var1 or CategoryPage_ListWithoutOptions_Var2
-        //tags = "CategoryPage_CompactList_Var1 or @CategoryPage_CompactList_Var2
+        //tags = "@CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3",
+        tags = "@CategoryPage_ListWithoutOptions_Var1 or CategoryPage_ListWithoutOptions_Var2",
+        //tags = "@CategoryPage_CompactList_Var1 or @CategoryPage_CompactList_Var2
 
-        //tags = "ProductPage_Var1 or @ProductPage_Var2 or @ProductPage_Var3
+        //tags = "@ProductPage_Var1 or @ProductPage_Var2 or @ProductPage_Var3
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json"}
 )
 public class CucumberRunnerTest {

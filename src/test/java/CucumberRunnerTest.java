f648af8be8731b169190944ceeccdc10f968581e
TarasYan <wildmonach@gmail.com>
2025-09-02 12:08:15 +0300

    Made some improvements for product block "Grid".

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -7,13 +7,13 @@ import io.cucumber.junit.Cucumber;
         features = "src/test/resources/features",
         glue = {"hooks", "steps"},
         //tags = "@CategoryPage_ListWithoutOptions_Var2",
-        tags = "@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3",
+        //tags = "@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3",
         //tags = "@ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3",
         //tags = "@ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3",
 
         //tags = "@BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3",
 
-        //tags = "@CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3",
+        tags = "@CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3",
         //tags = "@CategoryPage_ListWithoutOptions_Var1 or @CategoryPage_ListWithoutOptions_Var2",
         //tags = "@CategoryPage_CompactList_Var1 or @CategoryPage_CompactList_Var2",
 

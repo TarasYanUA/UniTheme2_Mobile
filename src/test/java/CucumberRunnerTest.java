957c732268a6a1dfa36b08d658b19fcc8687f1fd
TarasYan <wildmonach@gmail.com>
2025-10-01 14:03:59 +0300

    created packages for all test classes

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -6,16 +6,19 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"hooks", "steps"},
-        tags = "@CategoryPage_Grid_Var3",
+        //tags = "@ProductPage_Var1",
+
         //tags = "@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3",
         //tags = "@ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3",
         //tags = "@ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3",
 
-        //tags = "@BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3",
+        tags = "@BottomStickyPanel_Var1 or @BottomStickyPanel_Var2 or @BottomStickyPanel_Var3",
 
         //tags = "@CategoryPage_Grid_Var1 or @CategoryPage_Grid_Var2 or @CategoryPage_Grid_Var3",
         //tags = "@CategoryPage_ListWithoutOptions_Var1 or @CategoryPage_ListWithoutOptions_Var2",
         //tags = "@CategoryPage_CompactList_Var1 or @CategoryPage_CompactList_Var2",
+
+        //tags = "@ProductPage_Var1 or @ProductPage_Var2 or @ProductPage_Var3",
 
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json"}
 )

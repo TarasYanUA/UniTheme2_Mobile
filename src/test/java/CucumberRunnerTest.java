89bd4c811e913f6c9ea3f5a652609e69f18d4722
TarasYan <wildmonach@gmail.com>
2026-07-03 17:13:49 +0300

    The second test-case for banners is ready - ProductsAtBanners_Grid_2Columns

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"hooks", "steps"},
-        tags = "@HorizontalFilters",
+        tags = "@ProductsAtBanners_Grid_2Columns",
         //tags = "@ProductBlock_GridMore_Var1 or @ProductBlock_GridMore_Var2 or @ProductBlock_GridMore_Var3",
         //tags = "@ProductBlock_Scroller_Var1 or @ProductBlock_Scroller_Var2 or @ProductBlock_Scroller_Var3",
         //tags = "@ProductBlock_SmallItems_Var1 or @ProductBlock_SmallItems_Var2 or @ProductBlock_SmallItems_Var3",
@@ -21,6 +21,8 @@ import io.cucumber.junit.Cucumber;
         //tags = "@ProductPage_Var1 or @ProductPage_Var2 or @ProductPage_Var3",
 
         //tags = "@ButtonForVerticalMenu",
+
+        //tags = "ProductsAtBanners_Grid_1Column" or "ProductsAtBanners_Grid_2Columns",
 
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json"}
 )
